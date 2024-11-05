4f48c76ee5a4aa42b96c53b1518abbf5a56dc179
Junaid PV <junu.pv@gmail.com>
2024-11-05 13:32:44 +0530

    Sender has tab id!

--- src/background.js
+++ src/background.js
@@ -3,15 +3,13 @@ var imeLanguageRulesPath = 'libs/jquery.ime/';
 
 chrome[runtimeOrExtension].onMessage.addListener( function ( request, sender, sendResponse ) {
 	if ( request.fileToInject !== undefined ) {
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-			chrome.scripting.executeScript({ 
-				files: [imeLanguageRulesPath + request.fileToInject],
-				world: chrome.scripting.ExecutionWorld.ISOLATED,
-				target: {allFrames: true, tabId: tabs[0].id}
-			}, function () {
-				sendResponse( { 'injected': true } );
-			} );
-		});
+		chrome.scripting.executeScript({
+			files: [imeLanguageRulesPath + request.fileToInject],
+			world: chrome.scripting.ExecutionWorld.ISOLATED,
+			target: {allFrames: true, tabId: sender.tab.id}
+		}, function () {
+			sendResponse( { 'injected': true } );
+		} );
 	}
 	else {
 		sendResponse( { 'injected': false, 'errorMessage': 'No file specified' } );
